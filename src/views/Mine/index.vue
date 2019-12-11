aa61a17ea891c6d781efdf74abe5dddd5572ae7f
a1848003515 <1848003515@qq.com>
2019-12-11 20:25:56 +0800

    add setData

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="main">
+        <Header title="我的喵喵" />
+        <div id="content">
+            <Login />
+        </div>
+        <TabBar />
+    </div>
+</template>
+
+
+<script>
+import Header from '@/components/Header';
+import TabBar from '@/components/TabBar';
+import Login from '@/components/Login';
+
+export default {
+    name : 'Mine',
+    components :{
+        Header,
+        TabBar,
+        Login
+    }
+}
+</script>
+
+<style scoped>
+#content{ flex:1; overflow:auto; margin-bottom: 50px; position: relative; display: flex; flex-direction:column;}
+
+</style>
